20739d5c1fee8be3ddb02776d9a17600ada88675
이현빈 <lhb0107@naver.com>
2024-12-24 18:38:37 +0900

    초안 완성 피드백해주세요

---
+++ src/app/(total)/layout.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="">
+      <header className="flex gap-6 items-center justify-between sticky top-0 w-full z-50 box-border py-6 px-20 h-20 bg-white shadow-md">
+        <Link href={"/"} className="font-bold text-4xl font-sans">
+          3HP       
+          {/* 더꾸미기 */}
+        </Link>
+        <section className="flex gap-8">
+          <Link href={"/"}>HOME</Link>
+          <Link href={"/"}>딥페이크 방지</Link>
+          <Link href={"/"}>미정</Link>
+          <Link href={"/"}>미정</Link>
+        </section>
+        <section className="flex gap-9 items-center">
+          <Link href={"/"}>다른 기능</Link>
+          <Button variant="outline" className="bg-black text-white">
+            Button
+          </Button>
+        </section>
+      </header>
+      {children}
+      <footer className="bg-gray-400"> 
+        @3HP제작
+        {/* 푸터 더 꾸미기 */}
+      </footer>
+    </div>
+  );
+}
---
+++ src/app/(total)/page.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export default function Home() {
+  return (
+    <>
+      <div className="grid place-items-center h-screen bg-gradient-to-t from-gray-400 to-white p-8 sm:p-20 font-[var(--font-geist-sans)]">
+        <div className="text-center space-y-4">
+          <h1 className="text-4xl font-bold text-gray-800">
+            딥페이크 방지하는 좋은 선택, 3HP
+          </h1>
+          <p className="text-lg text-gray-600">
+            인물 이미지, 생성 이미지의 딥페이크를 막아주는 3HP
+          </p>
+          <Button variant="outline" className="bg-black text-white">
+            체험하러 가기
+          </Button>
+        </div>
+        <Image
+          src=""
+          alt="딥페이크 방지"
+          width={500}
+          height={300}
+          className="rounded-lg shadow-lg"
+        />
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>정말 딥페이크 방지가 가능한가요?1</AccordionTrigger>
+            <AccordionContent>
+              아직 미정1
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>정말 딥페이크 방지가 가능한가요?2</AccordionTrigger>
+            <AccordionContent>
+              아직 미정2
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </>
+  );
+}
